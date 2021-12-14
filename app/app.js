33d44f443b5c52da2397dfbc94f151aa899f48d2
Shahenda-Magdy <shahendamagdymohammed54@gmail.com>
2021-12-15 00:26:52 +0200

    initial commit

---
+++ app/app.js
@@ -0,0 +1,79 @@
+var express = require('express');
+var path = require('path');
+var app = express();
+var fs = require('fs');
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+//mongoose connection
+async function main(){
+  var mongoose = require('mongoose');
+
+  var url = 'mongodb+srv://admin:admin@cluster0.uhgcb.mongodb.net/mydb?retryWrites=true&w=majority';
+  mongoose.connect(url, {useNewUrlParser: true, useUnifiedTopology: true});
+  
+  db = mongoose.connection;
+
+  db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+}
+main().catch(console.sever);
+
+// users
+var u = fs.readFileSync('users.json');
+var obj = JSON.parse(u);
+
+console.log(obj.username);
+
+let data = {
+  "list": [
+    {username:"noha","password":"abc"},
+    {"username":"shahenda","password":"abc"},
+    {"username":"ahmed","password":"abc"}
+ ]}
+
+ for (var x in obj) {
+   if(obj[x].username =="noha"){
+    console.log('found')
+ }
+}
+
+	app.get('/', function(req, res){
+		res.render('index', {tittle: "express"})
+	  });
+//     app.get('/', function(req, res){
+// 	res.cookie("userData", obj);
+// 	res.send('user data added to cookie')
+//   alert("cookie saved")
+// });
+app.post('/', function(req, res){
+  
+  var name = req.body.username;
+  var pass = req.body.password;
+
+  if(data.list.find( record => record.username === name)){
+    if(data.list.find( record => record.password === pass)){
+      res.cookie("userData", obj);
+      res.render('home',{tittle: "express"})
+    }
+  }
+  else{
+    console.log('user not found!');
+  }
+});
+app.post('home',function(req,res){
+res.render('blog')
+});
+app.get('/logout', (req, res)=>{
+	//it will clear the userData cookie
+	res.clearCookie('userData');
+	res.send('user logout successfully');
+	});
+
+
+app.listen(3000)
---
+++ app/index.js
@@ -0,0 +1,21 @@
+// Importing express module
+const express = require("express")
+
+// Importing express-session module
+const session = require("express-session")
+
+// Importing file-store module
+const filestore = require("session-file-store")(session)
+
+const path = require("path")
+
+// Setting up the server
+var app = express()
+
+// Creating session
+
+
+// Server setup
+app.listen(3000, () => {
+	console.log("Server is Starting")
+})
